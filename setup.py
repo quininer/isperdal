e337766a68073578d61b921fc7b1783ec95faa1c
quininer <quininer@live.com>
2015-07-06 17:11:03 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+from os import path
+
+setup(
+    name='isperdal',
+    version='0.1',
+    description='a web framework.',
+    author='quininer kel',
+    author_email='quininer@live.com',
+    url='https://github.com/quininer/isperdal',
+    license='MIT',
+    long_description=open(path.join(path.split(path.abspath(__file__))[0], 'README.md')).read(),
+    packages=['isperdal'],
+    requires=['aiohttp']
+)
