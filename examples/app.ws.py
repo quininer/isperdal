68702177dc7eb33e5dc0db05cc7dbca8e4a9652d
quininer <quininer@live.com>
2015-08-09 12:06:40 +0800

    ws examples

---
+++ examples/app.ws.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import asyncio
+
+from isperdal import Microwave as u
+from isperdal.websocket import WebSocket
+
+app = u('/')
+
+@app.all()
+class Ws(WebSocket):
+    @asyncio.coroutine
+    def on_message(self, message):
+        self.writer.send(message)
+
+app.run()
