9440bc7ccd1f8c00363f5eb127fc8f5457e621df
quininer <quininer@live.com>
2015-11-17 22:51:52 +0800

    remove only

--- examples/app.async.py
+++ examples/app.async.py
@@ -2,13 +2,11 @@
 # encoding: utf-8
 
 from isperdal import Node as u
-from isperdal import only
 
 app = u('/')
 
 
 @app.get(*map(u, ('', "index")))
-@only
 async def index(this, req, res):
     return res.push(
         await req.query("id")
